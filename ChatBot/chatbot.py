9289d65dc7ccdf7d35543fe363a2b45cb155bf5f
Skripicina Albina <a.b.skrypitsyna@student.khai.edu>
2025-11-23 17:40:08 +0200

    ChatBot 2-nd stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,7 @@
+bot_name = "Charlie"
+birth_year = "2025"
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {birth_year}.")
+print("Please, remind me your name.")
+your_name = input(">")
+print(f"What a great name you have, {your_name}!")
