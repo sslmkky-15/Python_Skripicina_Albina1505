7ba3c7e3efc957181cb0b864b4571d5cd76ec8df
Skripicina Albina <a.b.skrypitsyna@student.khai.edu>
2025-11-23 18:24:23 +0200

    ChatBot 4-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -12,3 +12,8 @@ remainder5 = int(input(">"))
 remainder7 = int(input(">"))
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print(f"Your age is {age}; that's a good time to start programming!")
+print("Now I will prove to you that I can count to any number you want.")
+count_to = int(input())
+for i in range(count_to + 1):
+    print(f"{i}!")
+print("Completed, have a nice day!")
