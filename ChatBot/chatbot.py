c883341cb9b0989f59db1fa1fdea68571001b25d
Skripicina Albina <a.b.skrypitsyna@student.khai.edu>
2025-11-23 18:40:47 +0200

    ChatBot 5-th stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -13,7 +13,21 @@ remainder7 = int(input(">"))
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 print(f"Your age is {age}; that's a good time to start programming!")
 print("Now I will prove to you that I can count to any number you want.")
-count_to = int(input())
+count_to = int(input(">"))
 for i in range(count_to + 1):
     print(f"{i}!")
 print("Completed, have a nice day!")
+print("Let's test your programming knowledge.")
+print("Why do we use methods?")
+print("1. To repeat a statement multiple times.")
+print("2. To decompose a program into several small subroutines.")
+print("3. To determine the execution time of a program.")
+print("4. To interrupt the execution of a program.")
+correct_answer = "2"
+user_answer = ""
+while user_answer != correct_answer:
+    user_answer = input("> ")
+    if user_answer != correct_answer:
+        print("Please, try again.")
+print("Completed, have a nice day!")
+print("Congratulations, have a nice day!")
