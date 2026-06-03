48023f02537a90f5e069f5d6b2df4bfd6bb5f02e
Skripicina Albina <a.b.skrypitsyna@student.khai.edu>
2026-06-03 21:00:39 +0300

    Оновив пошук тіла статті та додав спецсимволи до input

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -5,10 +5,9 @@ from bs4 import BeautifulSoup
 
 
 def scrape_nature_articles():
-
     try:
-        num_pages = int(input())
-        article_type = input()
+        num_pages = int(input("> "))
+        article_type = input("> ")
     except ValueError:
         print("Invalid input")
         return
@@ -16,9 +15,7 @@ def scrape_nature_articles():
     base_url = "https://www.nature.com/nature/articles?sort=PubDate&year=2022&page="
     domain = "https://www.nature.com"
 
-
     for page_num in range(1, num_pages + 1):
-
         folder_name = f"Page_{page_num}"
         if not os.path.exists(folder_name):
             os.mkdir(folder_name)
@@ -31,39 +28,35 @@ def scrape_nature_articles():
             continue
 
         soup = BeautifulSoup(response.content, 'html.parser')
-
-
         articles = soup.find_all('article')
 
         for article in articles:
-
             span_type = article.find('span', {'data-test': 'article.type'})
 
             if span_type and span_type.text.strip() == article_type:
-
                 a_tag = article.find('a', {'data-track-action': 'view article'})
+
                 if not a_tag:
                     continue
 
                 article_link = domain + a_tag.get('href')
                 raw_title = a_tag.text.strip()
 
-
                 trans_table = str.maketrans('', '', string.punctuation)
                 clean_title = raw_title.translate(trans_table).replace(' ', '_')
+
                 file_name = f"{clean_title}.txt"
                 file_path = os.path.join(folder_name, file_name)
-
 
                 article_resp = requests.get(article_link, headers={'Accept-Language': 'en-US,en;q=0.5'})
                 if article_resp.status_code == 200:
                     article_soup = BeautifulSoup(article_resp.content, 'html.parser')
 
+                    # Оновлений пошук тіла статті згідно з коментарем
+                    body = article_soup.find("p", class_="article__teaser")
 
-                    body_div = article_soup.find('div', class_=lambda c: c and 'body' in c.lower())
-
-                    if body_div:
-                        article_text = body_div.text.strip()
+                    if body:
+                        article_text = str(body.text).strip()
 
                         with open(file_path, 'wb') as file:
                             file.write(article_text.encode('utf-8'))
